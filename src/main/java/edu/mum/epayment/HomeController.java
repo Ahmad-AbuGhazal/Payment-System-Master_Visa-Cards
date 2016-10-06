10d515afe2ee2bdbf0d3429c65daf55ba2771a4d
happybubblefish <congls2007@yahoo.com>
2016-10-06 15:44:01 -0400

    Generate model

--- src/main/java/edu/mum/epayment/HomeController.java
+++ src/main/java/edu/mum/epayment/HomeController.java
@@ -1,4 +1,4 @@
-package epayment;
+package edu.mum.epayment;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/edu/mum/model/Master.java
@@ -0,0 +1,75 @@
+package edu.mum.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Master {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String cardHoler;
+	
+	private String cardNum;
+	
+	private String securityCode;
+	
+	private Date expiration;
+	
+	private boolean status;
+	
+	private Address address;
+
+	public String getCardHoler() {
+		return cardHoler;
+	}
+
+	public void setCardHoler(String cardHoler) {
+		this.cardHoler = cardHoler;
+	}
+
+	public String getCardNum() {
+		return cardNum;
+	}
+
+	public void setCardNum(String cardNum) {
+		this.cardNum = cardNum;
+	}
+
+	public String getSecurityCode() {
+		return securityCode;
+	}
+
+	public void setSecurityCode(String securityCode) {
+		this.securityCode = securityCode;
+	}
+
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
