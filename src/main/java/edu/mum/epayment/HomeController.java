cffbf303414347cea4a51ec98b194681af758692
happybubblefish <congls2007@yahoo.com>
2016-10-07 14:27:07 -0400

    Finished controller part for verifying credit card.

--- src/main/java/edu/mum/epayment/HomeController.java
+++ src/main/java/edu/mum/epayment/HomeController.java
@@ -1,10 +1,22 @@
 package edu.mum.epayment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.mum.model.RequestedCard;
+import edu.mum.model.ResponseInfo;
+import edu.mum.service.CreditCardService;
+import scala.annotation.meta.setter;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private CreditCardService creditCardService;
 
 	@RequestMapping(value="/")
 	public String homePage(){
@@ -12,6 +24,13 @@ public class HomeController {
 		return "index";
 	}
 	
-	
+	@RequestMapping(value = "verifycard", method = RequestMethod.GET)
+	public @ResponseBody ResponseInfo verifyCard(@RequestBody RequestedCard requestedCard){
+		ResponseInfo responseInfo = new ResponseInfo();
+		char response = creditCardService.verifyCreditCard(requestedCard);
+		responseInfo.setResponse(response);
+		
+		return responseInfo;
+	}
 }
 
---
+++ src/main/java/edu/mum/model/ResponseInfo.java
@@ -0,0 +1,13 @@
+package edu.mum.model;
+
+public class ResponseInfo {
+	private char response;
+
+	public char getResponse() {
+		return response;
+	}
+
+	public void setResponse(char response) {
+		this.response = response;
+	}
+}
