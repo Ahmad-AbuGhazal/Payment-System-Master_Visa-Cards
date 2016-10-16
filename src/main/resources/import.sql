c7ef389ee0f40bc1bc14dfdef9a460856f5d9837
happybubblefish <congls2007@yahoo.com>
2016-10-16 01:14:01 -0400

    Reorder the statement in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,20 @@
 insert into dbo.Visa (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Lin Cong', '4024007106035073', '123', '2018-07-26', 5000.00, 3000.96, 1);
 
-insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Weiwei Wu', '5213377999854270', '123', '2018-08-26', 4000.00, 2000.25, 1);
-
-insert into dbo.Visa (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Jannatul Mou', '4539219547584753', '567', '2018-09-26', 6000.00, 5000.96, 1);
-
-
-insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Ahmad Abu Ghazal', '5388531505591888', '567', '2018-10-26', 8000.00, 4000.25, 1);
-
 insert into dbo.Visa (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Mihir Palakkurthi', '4716076553785434', '891', '2018-11-28', 5000.00, 3000.96, 1);
-
-insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Tek Bahadur', '5113164815700916', '891', '2018-12-28', 8000.00, 2000.25, 1);
 
 insert into dbo.Visa (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Alfarasi Adjis', '4485719797298909', '777', '2018-07-30', 5000.00, 3000.96, 1);
 
-insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Kidist', '5380249490045764', '919', '2019-08-30', 7000.00, 2000.25, 1);
-
 insert into dbo.Visa (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Rejina Srestha', '4929396237587656', '555', '2018-12-26', 5000.00, 3000.96, 1);
+
+insert into dbo.Visa (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Jannatul Mou', '4539219547584753', '567', '2018-09-26', 6000.00, 5000.96, 1);
+
+insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Weiwei Wu', '5213377999854270', '123', '2018-08-26', 4000.00, 2000.25, 1);
+
+insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Ahmad Abu Ghazal', '5388531505591888', '567', '2018-10-26', 8000.00, 4000.25, 1);
+
+insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Tek Bahadur', '5113164815700916', '891', '2018-12-28', 8000.00, 2000.25, 1);
+
+insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Kidist', '5380249490045764', '919', '2019-08-30', 7000.00, 2000.25, 1);
 
 insert into dbo.Master (cardHolder, cardNum, securityCode, expiration, maxCredit, availableCredit, status) values ('Sabanam Lakhey', '5170509833147344', '999', '2018-06-26', 9000.00, 6000.25, 1);
 
